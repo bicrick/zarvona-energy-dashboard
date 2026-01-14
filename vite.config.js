1c0000f4e87091e899ff898efbee1ed140d0e1b4
Patrick Brown <brown.patrick@heb.com>
2026-01-14 16:32:39 -0600

    changed vite build path

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
 
-export default defineConfig({
-    base: '/zarvona-energy-dashboard/',
+export default defineConfig(({ command }) => ({
+    base: command === 'build' ? '/zarvona-energy-dashboard/' : '/',
     build: {
         outDir: 'docs'
     },
     server: {
         port: 5173
     }
-});
+}));
